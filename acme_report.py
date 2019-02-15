71a2d01db8837b9e80a6e2c8fda300624e68d240
Carlos Gutierrez <chgutier@gmail.com>
2019-02-15 15:53:16 -0600

    Add files via upload

--- acme_report.py
+++ acme_report.py
@@ -1,39 +1,39 @@
 from acme import Product
 
 import random
-
-def generate_products(30):
+ADJECTIVES = ['Awesome', 'Shiny', 'Impressive', 'Portable', 'Improved']
+NOUNS = ['Anvil', 'Catapult', 'Disguise', 'Mousetrap', '???']
+def generate_products(n=30):
     product_lst = []
-    for _ range(30):
-        adjective = random.choice(['Awesome', 'Shiny', 'Impressive', 'Portable', 'Improved'])
-        noun = random.choice(['Anvil', 'Catapult' 'Disguise' 'Mousetrap', '???'])
-        price_rand = randint(5, 100)
-        weight_rand = randint(5, 100)
+    for _ in range(n):
+        adj_rand = random.choice(ADJECTIVES)
+        noun_rand= random.choice(NOUNS)
+        price_rand = random.randint(5, 100)
+        weight_rand = random.randint(5, 100)
         flam_rand = random.uniform(0.0, 2.5)
         
-        prod = Product(name=f'{adjective} {noun}', price=price_rand, weight=weight_rand, flammability=flam_rand)
-
-    product_lst.append([prod.name, prod.price, prod.weight, prod.flammability])
+        prod = Product(name=f'{adj_rand} {noun_rand}', price=price_rand, weight=weight_rand, flammability=flam_rand)
+        product_lst.append([prod.name, prod.price, prod.weight, prod.flammability])
     
     return product_lst
 
-    return 
-
-def inventory_report(generate_products):
-    for each in product_lst:
-        names_cnt = len(product_lst)
-        avg_price = 
+def inventory_report(product_lst):
+    for x in product_lst:
+        names_cnt = len(set([x[0] for x in product_lst]))
+        avg_price = sum ([x[1] for x in product_lst]) / len(product_lst)
+        avg_weight = sum ([x[2] for x in product_lst]) / len(product_lst)
+        avg_flam = sum ([x[3] for x in product_lst]) / len(product_lst)
 
     print('ACME CORPORATION OFFICIAL INVENTORY REPORT')
     print(f'Unique product names: {names_cnt}')
-    print(f'Average price: {}')
-    print(f'Average weight: {}')
-    print(f'Average flammability: {}')
+    print(f'Average price: {avg_price}')
+    print(f'Average weight: {avg_weight}')
+    print(f'Average flammability: {avg_flam}')
 
 
 
 
-python
+#python
 if __name__ == '__main__':
     inventory_report(generate_products())
     
--- acme_test.py
+++ acme_test.py
@@ -1,4 +1,4 @@
-python
+#python
 #!/usr/bin/env python
 
 import unittest
@@ -12,6 +12,33 @@ class AcmeProductTests(unittest.TestCase):
         """Test default product price being 10."""
         prod = Product('Test Product')
         self.assertEqual(prod.price, 10)
+
+    def test_stealability(self):
+        prod = Product('Tester', 19, 8, 0.8)
+        self.assertEqual(prod.stealability(), "Very stealable!")
+    
+    def test_explode(self):
+        prod = Product('Tester', 19, 8, 0.8)
+        self.assertEqual(prod.explode(), "...fizzle.")
+
+
+
+class AcmeReportTests(unittest.TestCase):
+    
+    def test_default_num_products(self):
+        """Checking that that list of length 30"""
+        prod_lst = generate_products(30)
+        self.assertEqual(len(prod_lst), 30)
+
+    def test_legal_names(self):
+        '''Checks that the generated names for a
+        default batch of products are all valid'''
+        from itertools import product
+        combos = list(product(ADJECTIVES, NOUNS))
+        name_combos = [' '.join(combos[i]) for i in range(len(combos))]
+        prod_lst = generate_products(30)
+        name_lst = [x[0] for x in prod_lst]
+        self.assertTrue(name_combos, name_lst)
 
 
 if __name__ == '__main__':
